0b9bb2ec63567cba59c425081ff9a694cd6ddf25
Char Howland <char@indicio.tech>
2021-08-31 10:12:42 -0400

    fix: increase timeouts on test_send_cred_def
    Signed-off-by: Char Howland <char@indicio.tech>

--- int/tests/test_credentialdefinitions.py
+++ int/tests/test_credentialdefinitions.py
@@ -52,7 +52,7 @@ async def test_cred_def_get(connection, endorser_did, create_schema):
             "schema_id": schema.sent.schema_id,
             "~transport": {"return_route": "all"},
         },
-        timeout=30,
+        timeout=50,
     )
     cred_def_get = await connection.send_and_await_reply_async(
         {
@@ -79,7 +79,7 @@ async def test_cred_def_get_list(connection, endorser_did, create_schema):
             "schema_id": schema1_2.sent.schema_id,
             "~transport": {"return_route": "all"},
         },
-        timeout=30,
+        timeout=50,
     )
     schema1_3 = await create_schema(version="1.3")
     send_schema1_3 = await connection.send_and_await_reply_async(
